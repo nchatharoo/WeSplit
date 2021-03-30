3089e0335e0ba15429af499188c49bfebbb1e447
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-03-30 21:13:45 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nadheer on 30/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
