a8832ab4a13aa41abc4513f40070b7256e76bbbd
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-04-01 13:35:57 +0200

    Creating pickers for numbers of people and tip percentage

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,15 +15,32 @@ struct ContentView: View {
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
-            }
-            
-            Section {
-                Text("$\(checkAmount)")
-            }
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
+            }.navigationBarTitle("WeSplit")
         }
     }
 }
